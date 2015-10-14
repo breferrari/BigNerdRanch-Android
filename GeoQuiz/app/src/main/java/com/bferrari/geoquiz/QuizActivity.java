9f0ec30654b27acb69ae635af7a33cc166f80853
Brenno Ferrari <brennoferrari@gmail.com>
2015-10-13 23:57:44 -0300

    GeoQuiz Code Cleanup

--- GeoQuiz/app/src/main/java/com/bferrari/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bferrari/geoquiz/QuizActivity.java
@@ -1,8 +1,7 @@
 package com.bferrari.geoquiz;
 
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
